7d1ea713b679c9f5932cde32e1e80f45c90ee290
Oleg Maslov <maslovoa.ge@gmail.com>
2024-07-10 20:01:47 +0200

    add bot cart, add search in list

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from product.apps import ProductConfig
-from product.views import bot_list
+from product.views import BotListView, BotDetailView
 
 app_name = ProductConfig.name
 
 urlpatterns = [
-    path('', bot_list, name='list'),
+    path('', BotListView.as_view(), name='list'),
+    path('<int:pk>/', BotDetailView.as_view(), name='get'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,10 +1,29 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from product.models import Bot
 
 
-def bot_list(request):
-    context = {
-        'object_list': Bot.objects.all()
-    }
-    return render(request, 'product/bot_list.html', context)
+# def bot_list(request):
+#     context = {
+#         'object_list': Bot.objects.all()
+#     }
+#     return render(request, 'product/bot_list.html', context)
+
+
+class BotListView(ListView):
+    model = Bot
+    # paginate_by = 1
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('q')
+        if search_query:
+            queryset = queryset.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
+        return queryset
+
+
+class BotDetailView(DetailView):
+    model = Bot
+    # template_name = 'product/bot_detail.html'
